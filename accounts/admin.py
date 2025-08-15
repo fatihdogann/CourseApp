fe5221ac550d8ca8ffa0e662939bd7785a0e5fe4
fatihdogann <fatihdogann5@outlook.com>
2025-08-15 21:28:25 +0300

    Finalize project structure: Update admin files and prepare for GitHub

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# Register the User model with the default UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, BaseUserAdmin)
